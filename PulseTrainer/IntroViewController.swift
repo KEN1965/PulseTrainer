5b745a663f6eddcf87b72c520ecc3eae53fad465
Kenichi Takahama <hammer528@msn.com>
2021-04-06 14:55:20 +0900

    first commit

---
+++ PulseTrainer/IntroViewController.swift
@@ -0,0 +1,10 @@
+//
+//  IntroView.swift
+//  PulseTrainer
+//
+//  Created by KEN on 2021/03/30.
+//
+
+import UIKit
+
+
---
+++ PulseTrainer/SetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SetTableViewCell.swift
+//  PulseTrainer
+//
+//  Created by KEN on 2021/03/28.
+//
+
+import UIKit
+
+class SetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
