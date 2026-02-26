4410f05ce85b9ceae0a791b41a0d00b811dfd02b
KEN1965 <hammer528@msn.com>
2026-02-26 09:24:52 +0900

    Initial Commit

---
+++ PulseTrainer/PulseTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseTrainerApp.swift
+//  PulseTrainer
+//
+//  Created by Kenichi Takahama on 2026/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
